5d8b2434943ecbafb1910037627380188b141515
Jae_Yu <ryooju0114@gmail.com>
2020-10-23 18:32:06 +0900

    수정

--- Level_1/Lv1_자연수_뒤집어_배열로_만들기.java
+++ Level_1/Lv1_자연수_뒤집어_배열로_만들기.java
@@ -7,7 +7,19 @@ public class Lv1_자연수_뒤집어_배열로_만들기 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int length = Long.toString(n).length();
+        int[] answer = new int[length];
+        int index = 0;
+        
+        while(n > 0){
+            answer[index] = (int)(n%10);
+            
+            n /= 10;
+            index ++;
+        }
+        
+        System.out.println(answer);
+        return answer;
 	}
 
 }
