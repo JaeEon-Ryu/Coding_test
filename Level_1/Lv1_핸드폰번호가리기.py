a22baa576d7caef6c26f4071147a97435f29d650
Jae_Yu <ryooju0114@gmail.com>
2020-10-31 14:48:18 +0900

    코드수정

--- Level_1/Lv1_핸드폰번호가리기.py
+++ Level_1/Lv1_핸드폰번호가리기.py
@@ -1,12 +1,6 @@
 def solution(phone_number):
-    answer = ''
-    for idx,i in enumerate(phone_number):
-        if idx < 4:
-            answer += '*'
-        else:
-            answer += i
+    return '*'*(len(phone_number)-4) + phone_number[-4:]
 
-    return answer
 
 phone_number = "01033334444	"
 print(solution(phone_number))
