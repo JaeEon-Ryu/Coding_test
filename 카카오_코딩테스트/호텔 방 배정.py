7ae49ddab36616e1d8dbd57305d94881313c360a
Jae_Yu <ryooju0114@gmail.com>
2020-11-02 01:26:46 +0900

    수정완료

--- 카카오_코딩테스트/호텔 방 배정.py
+++ 카카오_코딩테스트/호텔 방 배정.py
@@ -3,16 +3,41 @@ def solution(k, room_number):
     room_dict = dict()
 
     for number in room_number:
+        visited = [number]
 
         while True:
             if room_dict.get(number) == None:
-                room_dict[number] = number + 1
                 result.append(number)
                 break
             else:
                 number = room_dict[number]
+                visited.append(number)
+
+        for _ in visited:
+            room_dict[_] = number + 1
 
     return result
+
+
+
+# def solution(k, room_number):
+#     result = []
+#     room_dict = dict()
+#
+#     for number in room_number:
+#
+#         while True:
+#
+#             if room_dict.get(number) == None:
+#                 room_dict[number] = number + 1
+#                 result.append(number)
+#                 break
+#             else:
+#                 number = room_dict[number]
+#
+#
+#     return result
+
 
 ''' 정확성은 만점 but 효율성 0 점 
 def solution(k, room_number):
@@ -22,7 +47,7 @@ def solution(k, room_number):
         if number in result:
             sorted(result)
             number+=1
-            
+
             while number in result:
                 number += 1
             result.append(number)
@@ -34,5 +59,5 @@ def solution(k, room_number):
 '''
 
 k = 10
-room_number = [1,3,4,1,3,1]
-print(solution(k,room_number))
+room_number = [1, 3, 4, 1, 3, 1]
+print(solution(k, room_number))
