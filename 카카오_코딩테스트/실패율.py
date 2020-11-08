17044fa0d75a045ee54ddb75f08039125f8d1b47
Jae_Yu <ryooju0114@gmail.com>
2020-11-08 15:06:05 +0900

    코드 수정중

---
+++ 카카오_코딩테스트/실패율.py
@@ -0,0 +1,54 @@
+'''
+runtime error와 testcase 2개가 틀린 코드
+testcase의 경우 while문을 돌고 맨 마지막 숫자를 추가하는 과정에서
+생긴 문제 같음 -> 수정 가능
+
+runtrim error의 경우는 조금 더 찾아봐야할듯, 일단 리스트 인덱스부터
+조사하여 할당되지 않은 값까지 조사할 예정
+'''
+
+import bisect
+
+def solution(N, stages):
+    answer = []
+    stages.sort()
+    find_N_position = bisect.bisect(stages, N)
+    amount = len(stages) - find_N_position
+    stages = stages[:find_N_position]
+
+    is_not_cleared = 0
+    #print(stages)
+    while stages:
+        stage = stages.pop()
+        if N != stage:
+            if N-stage != 1:
+                for _ in range(N-stage-1):
+                    answer.append(0)
+                    N -= 1
+
+            answer.append(is_not_cleared/amount)
+            is_not_cleared = 1
+            N -= 1
+
+        else:
+            is_not_cleared += 1
+
+        amount += 1
+    answer.append(is_not_cleared / amount)
+
+    if N>1:
+        for _ in range(N-1):
+            answer.append(0)
+
+    answer = answer[::-1]
+    answer = sorted(range(len(answer)),key=answer.__getitem__,reverse=True)
+    for _ in range(len(answer)): answer[_] += 1
+
+    return answer
+
+# N = 5
+# stages = [2,1,2,6,2,4,3,3]
+
+N = 4
+stages = [4,4,4,4,4]
+print(solution(N,stages))
