13be8a2e5e1441178b2ab0be91944f0ed204f117
Jae_Yu <ryooju0114@gmail.com>
2021-02-26 14:16:25 +0900

    accepted

---
+++ LeetCode/0033_ Search in Rotated Sorted Array.py
@@ -0,0 +1,34 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # solution - built-in function
+        return nums.index(target) if target in nums else -1
+
+
+'''
+solution - binary search
+class Solution:
+    def search(self, A: List[int], target: int) -> int:
+        n = len(A)
+        left, right = 0, n - 1
+        if n == 0: return -1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if A[mid] == target: return mid
+
+            # inflection point to the right. Left is strictly increasing
+            if A[mid] >= A[left]:
+                if A[left] <= target < A[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            # inflection point to the left of me. Right is strictly increasing
+            else:
+                if A[mid] < target <= A[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
+'''
